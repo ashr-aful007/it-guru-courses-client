274a7897833713b8fc31a88c170de37daff91e7e
ashr-aful007 <ashraful99980@gmail.com>
2022-10-27 20:54:17 +0600

    add footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='bg-black mt-5 d-flex flex-column justify-content-center align-content-center h-100 w-100'>
+      <p className='text-bold fs-3 text-light w-20 mx-auto mt-3'>IT Guru</p>
+    </div>
   )
 }
 
